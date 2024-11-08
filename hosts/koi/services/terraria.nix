a259e63be90453d1b39f76d74d0a9d6c57c50e69
teidesu <alina@tei.su>
2024-11-08 22:22:56 +0300

    feat(koi): terraria server

---
+++ hosts/koi/services/terraria.nix
@@ -0,0 +1,11 @@
+{
+  services.terraria = {
+    enable = true;
+    maxPlayers = 10;
+    port = 7777;
+    messageOfTheDay = "penis gaming";
+    noUPnP = true;
+    openFirewall = true;
+    worldPath = "/srv/terraria/world.wld";
+  };
+}
